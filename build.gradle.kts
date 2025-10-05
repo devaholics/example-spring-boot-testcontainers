dd460a9b7cff7945d5b204e8a63543b3a672973e
Philipp Sommersguter <philipp@devaholics.io>
2025-10-05 10:33:24 +0200

    Reformat build.gradle.kts according to .editorconfig

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.5.6"
-	id("io.spring.dependency-management") version "1.1.7"
+  java
+  id("org.springframework.boot") version "3.5.6"
+  id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "io.devaholics"
@@ -9,36 +9,36 @@ version = "0.0.1-SNAPSHOT"
 description = "Demo project for Spring Boot with Testcontainers"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(25)
-	}
+  toolchain {
+    languageVersion = JavaLanguageVersion.of(25)
+  }
 }
 
 repositories {
-	mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-    implementation("org.springframework.boot", "spring-boot-starter-actuator")
-    implementation("org.springframework.boot", "spring-boot-starter-web")
+  implementation("org.springframework.boot", "spring-boot-starter-actuator")
+  implementation("org.springframework.boot", "spring-boot-starter-web")
 
-    implementation("org.springframework.boot:spring-boot-starter-mail")
-    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
-    implementation("org.postgresql", "postgresql")
+  implementation("org.springframework.boot:spring-boot-starter-mail")
+  implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+  implementation("org.postgresql", "postgresql")
 
-    // Note: It is important to put the testcontainers dependency in the "implementation" scope to be able to use it outside of testing sources!
-    implementation("org.springframework.boot:spring-boot-testcontainers")
-    implementation("org.testcontainers:postgresql")
+  // Note: It is important to put the testcontainers dependency in the "implementation" scope to be able to use it outside of testing sources!
+  implementation("org.springframework.boot:spring-boot-testcontainers")
+  implementation("org.testcontainers:postgresql")
 
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.testcontainers:junit-jupiter")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+  testImplementation("org.springframework.boot:spring-boot-starter-test")
+  testImplementation("org.testcontainers:junit-jupiter")
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+  useJUnitPlatform()
 }
 
 tasks.bootRun {
-    args("--spring.profiles.active=dev")
+  args("--spring.profiles.active=dev")
 }
