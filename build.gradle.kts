54397fe1ac7d80e51dc68248edb33e46674430a1
Philipp Sommersguter <philipp@devaholics.io>
2025-12-11 09:44:37 +0100

    Update to Gradle 9.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,8 +19,8 @@ repositories {
 }
 
 dependencies {
-  implementation("org.springframework.boot", "spring-boot-starter-actuator")
-  implementation("org.springframework.boot", "spring-boot-starter-web")
+  implementation("org.springframework.boot:spring-boot-starter-actuator")
+  implementation("org.springframework.boot:spring-boot-starter-web")
 
   implementation("org.springframework.boot:spring-boot-starter-mail")
   implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
